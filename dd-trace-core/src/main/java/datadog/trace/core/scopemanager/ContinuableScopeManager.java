def5494c99ac23978083a1ada13fe7c794f89a6e
Laplie Anderson <laplie.anderson@datadoghq.com>
2020-12-04 17:39:33 -0500

    Misbehaving ScopeListener should not affect others

--- dd-trace-core/src/main/java/datadog/trace/core/scopemanager/ContinuableScopeManager.java
+++ dd-trace-core/src/main/java/datadog/trace/core/scopemanager/ContinuableScopeManager.java
@@ -218,7 +218,11 @@ public class ContinuableScopeManager implements AgentScopeManager {
      */
     final void onProperClose() {
       for (final ScopeListener listener : scopeManager.scopeListeners) {
-        listener.afterScopeClosed();
+        try {
+          listener.afterScopeClosed();
+        } catch (Exception e) {
+          log.debug("ScopeListener threw exception in close()", e);
+        }
       }
     }
 
@@ -292,7 +296,11 @@ public class ContinuableScopeManager implements AgentScopeManager {
 
     public void afterActivated() {
       for (final ScopeListener listener : scopeManager.scopeListeners) {
-        listener.afterScopeActivated();
+        try {
+          listener.afterScopeActivated();
+        } catch (Exception e) {
+          log.debug("ScopeListener threw exception in afterActivated()", e);
+        }
       }
     }
   }
